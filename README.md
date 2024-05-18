2e78e52b32deeb7df5857aaeada52fcbd95d8217
Shenai Chan <73866558+shenaichan@users.noreply.github.com>
2024-05-17 21:03:10 -0500

    added wake word functionality, see readme for more details

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # hestia
 Hestia is a secure, customizable home automation assistant, second memory, and chatbot.
+
+5/17/2024:
+
+If using an M1 Mac, you may receive an error when installing pyaudio. In that case, you should use run `brew install portaudio` before trying again.
+
+According to picovoice, the wake word models (.ppn) are specifically created for Mac. I'm not sure if they still work on windows. It's worth giving it a shot. Otherwise, you'll need to make a free https://console.picovoice.ai/ account and "train" your own model from there because I'm already using 2/3 free model credits on my own lol. And then set your API key as PV_ACCESS_KEY in your env.
+
+Don't forget to install requirements.txt with pip3 install -r requirements.txt!
+
+Sometimes I would get a buffer overflow bug with the line pcm = audio_stream.read(porcupine.frame_length) so then I just set it to not raise an overflow error :sob:. I don't think it really matters since we're just constantly reading. Also, I stop reading during whatever we're executing on wake word detection, so maybe that will also help prevent buffer overflows because there's less time where the microphone is active but we're not emptying the buffer (shrugs).
+
+Also, since we have two wake words that have a shared "subphoneme (?)", right now I have a guard that prevents "hey hestia" from being read as both "hestia" and "hey hestia", just by asking "if you've already recognized a keyword in the last second, don't recognize another one yet". I don't think we'll need this once we're actually making a speech recognition API call because then the next read won't come until significantly later?
---
+++ src/main.py
@@ -0,0 +1,70 @@
+import pvporcupine
+import os
+import pyaudio
+import struct
+from datetime import datetime
+
+# for staggering keyword recognition
+prev_time = datetime.now()
+
+# params for porcupine initializer
+access_key = os.getenv('PV_ACCESS_KEY')
+keyword_paths=['wake_word_models/hestia_MAC.ppn', 
+               'wake_word_models/hey_hestia_MAC.ppn']
+
+# create objects to be populated later, if fail then they're available to be handled in the finally case
+porcupine = None
+audio_stream = None
+pa = None
+
+# debug var
+cnt = 0
+
+try:
+    # Initialize the Porcupine object
+    porcupine = pvporcupine.create(
+        access_key=access_key,
+        keyword_paths=keyword_paths
+    )
+
+    # Initialize PyAudio
+    pa = pyaudio.PyAudio()
+    audio_stream = pa.open(
+        rate=porcupine.sample_rate,
+        channels=1,
+        format=pyaudio.paInt16,
+        input=True,
+        frames_per_buffer=porcupine.frame_length 
+    )
+
+    print("Listening for wake word...")
+
+    while True:
+        pcm = audio_stream.read(porcupine.frame_length, exception_on_overflow=False)
+        # decode pcm into frame_length 16bit signed shorts
+        pcm = struct.unpack_from("h" * porcupine.frame_length, pcm)
+
+        keyword_index = porcupine.process(pcm)
+        
+        if keyword_index >= 0 and (datetime.now() - prev_time).seconds >= 1:
+            audio_stream.stop_stream()
+            prev_time = datetime.now()
+            if keyword_index == 0:
+                cnt += 1
+                print(str(cnt) + " hestia detected!")
+            elif keyword_index == 1:
+                cnt += 1
+                print(str(cnt) + " hey hestia detected!")
+            audio_stream.start_stream()
+
+except KeyboardInterrupt:
+    print("Stopping wake word detection")
+
+finally:
+    # Ensure resources are cleaned up
+    if porcupine is not None:
+        porcupine.delete()
+    if audio_stream is not None:
+        audio_stream.close()
+    if pa is not None:
+        pa.terminate()
